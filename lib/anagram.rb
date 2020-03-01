048212d1027f74670cb7f6f61725b5316c98adb1
Jake Garcia <bballwiz4@gmail.com>
2020-03-01 22:28:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,8 +12,6 @@ class Anagram
     matches = []
     words.each do |possible_anagram|
       possible_anagram_array = possible_anagram.split("")
-      print word_array.sort
-      print possible_anagram_array.sort
       if word_array.sort == possible_anagram_array.sort
         matches << possible_anagram
       end
